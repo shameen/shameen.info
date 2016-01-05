8e9e0aee7b5182fa4f1c0e998ac5b694f3cff47e
Shameen <shameen.azeez@googlemail.com>
2016-01-05 21:58:31 +0000

    add /work/ page and convert portfolio to JSON

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -9,7 +9,7 @@
         activate: function () {
             router.map([
                 { route: '', title:'Welcome', moduleId: 'viewmodels/welcome', nav: true },
-                { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true }
+                { route: 'work', moduleId: 'viewmodels/work', nav: true }
             ]).buildNavigationModel();
             
             return router.activate();
---
+++ app/viewmodels/work.js
@@ -0,0 +1,107 @@
+﻿define([], function () {
+    return {
+        displayName: 'Portfolio',
+        portfolioItems: [
+            {
+                title: "Hashtag Dungeon Website",
+                description: "indie game landing-page website",
+                img: null,
+                link: {
+                    url: "http://hashtagdungeon.com",
+                    active: true
+                },
+                tags: ["jQuery","CSS"]
+            },
+            {
+                title: "Negative",
+                description: "image processing tool for photo negatives (University final-year project)",
+                img: null,
+                link: {
+                    url: "https://github.com/shameen/negative",
+                    active: true
+                },
+                tags: ["MATLAB","GUI","Image Processing"]
+            },
+            {
+                title: "Timetabling System",
+                description: "database-driven ASP.NET timetabling system (University team project)",
+                img: null,
+                link: {
+                    url: null,
+                    active: false
+                },
+                tags: ["ASP.NET","SQL Server","Bootstrap"]
+            },
+            {
+                title: "Phil Wolstenholme Photography",
+                description: "multi-page website with self-made jquery photo gallery",
+                img: null,
+                link: {
+                    url: "http://philwolstenholme.com/",
+                    active: true
+                },
+                tags: ["jQuery","CSS"]
+            },
+            {
+                title: "Propah Job Studios Website",
+                description: "wordpress theme <small>(and previously, <a href='http://dl.dropbox.com/u/698380/propah/index.html'>simple HTML websites</a>)</small>",
+                img: null,
+                link: {
+                    url: "http://propahjobstudios.co.uk/",
+                    active: false
+                },
+                tags: ["WordPress","PHP","CSS"]
+            },
+            {
+                title: "snpt, online textbox",
+                description: "text storage and history <small>(<a href='http://snpt.shameen.info/beta'>beta</a>)</small>",
+                img: null,
+                link: {
+                    url: "http://snpt.shameen.info/",
+                    active: true
+                },
+                tags: ["PHP","MySQL"]
+            },
+            {
+                title: "Forbidden Memories Database",
+                description: "card game database browser and preview",
+                img: null,
+                link: {
+                    url: "http://fmr.shameen.info/",
+                    active: true
+                },
+                tags: ["PHP","MySQL","jQuery","jQuery UI"]
+            },
+            {
+                title: "FI Windows & Conservatories",
+                description: "basic website with gallery and effects",
+                img: null,
+                link: {
+                    url: "http://fiwindows.com/",
+                    active: false
+                },
+                tags: ["jQuery"]
+            },
+            {
+                title: "JR Ambulance Medical Resources",
+                description: "quote calculator",
+                img: null,
+                link: {
+                    url: "http://www.jr-ambulance.co.uk/calculator/medical_resources_calculator_AIO.html",
+                    active: false
+                },
+                tags: ["JavaScript"]
+            },
+            {
+                title: "iAlaina 30-second intro",
+                description: "3D main titles based on the the TV show 'iCarly'",
+                img: null,
+                link: {
+                    url: "http://vimeo.com/10457730",
+                    active: true
+                },
+                tags: ["3D","After Effects","Video","VFX"]
+            }
+        ]
+    };
+});
