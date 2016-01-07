031211a600e1d28f87f80c8a0c48c0f2ade6a87d
Shameen <shameen.azeez@googlemail.com>
2016-01-07 22:51:50 +0000

    /welcome contact links

--- Website/app/viewmodels/welcome.js
+++ Website/app/viewmodels/welcome.js
@@ -2,6 +2,23 @@
     var ctor = function () {
         this.title = 'Hello';
         this.subHeader = 'Nothing to see here.';
+        this.contactList = [
+            {
+                title: "Twitter",
+                img: "http://shameen.info/img/i_tw.png",
+                url: "http://twitter.com/shameen_"
+            },
+            {
+                title: "YouTube",
+                img: "http://shameen.info/img/i_yt.png",
+                url: "http://youtube.com/whatshameen"
+            },
+            {
+                title: "Github",
+                img: null,
+                url: "https://github.com/shameen"
+            }
+        ];
     };
     
     return ctor;
