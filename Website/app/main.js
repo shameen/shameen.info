239c3d514dbd913a6febc0f4149e1d2b3919072e
Shameen <shameen.azeez@googlemail.com>
2023-03-23 11:42:59 +0000

    fix build for node v12+, add postbuild to keep Website/lib updated

--- Website/app/main.js
+++ Website/app/main.js
@@ -1,39 +1,43 @@
 ﻿requirejs.config({
-    paths: {
-        'text': '../lib/require/text',
-        'durandal':'../lib/durandal/js',
-        'plugins' : '../lib/durandal/js/plugins',
-        'transitions' : '../lib/durandal/js/transitions',
-        'knockout': '../lib/knockout/knockout-3.1.0',
-        'bootstrap': '../lib/bootstrap/js/bootstrap',
-        'jquery': '../lib/jquery/jquery-1.9.1'
+  paths: {
+    text: "../lib/requirejs-text/text",
+    durandal: "../lib/durandal/js",
+    plugins: "../lib/durandal/js/plugins",
+    transitions: "../lib/durandal/js/transitions",
+    knockout: "../lib/knockout/build/output/knockout-latest",
+    bootstrap: "../lib/bootstrap/dist/js/bootstrap",
+    jquery: "../lib/jquery/jquery",
+  },
+  shim: {
+    bootstrap: {
+      deps: ["jquery"],
+      exports: "jQuery",
     },
-    shim: {
-        'bootstrap': {
-            deps: ['jquery'],
-            exports: 'jQuery'
-       }
-    }
+  },
 });
 
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
-    //>>excludeStart("build", true);
-    system.debug(true);
-    //>>excludeEnd("build");
+define(["durandal/system", "durandal/app", "durandal/viewLocator"], function (
+  system,
+  app,
+  viewLocator
+) {
+  //>>excludeStart("build", true);
+  system.debug(true);
+  //>>excludeEnd("build");
 
-    app.title = 'shameen.info';
+  app.title = "shameen.info";
 
-    app.configurePlugins({
-        router:true,
-        dialog: true
-    });
+  app.configurePlugins({
+    router: true,
+    dialog: true,
+  });
 
-    app.start().then(function() {
-        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-        //Look for partial views in a 'views' folder in the root.
-        viewLocator.useConvention();
+  app.start().then(function () {
+    //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+    //Look for partial views in a 'views' folder in the root.
+    viewLocator.useConvention();
 
-        //Show the app by setting the root view model for our application with a transition.
-        app.setRoot('viewmodels/shell', 'entrance');
-    });
+    //Show the app by setting the root view model for our application with a transition.
+    app.setRoot("viewmodels/shell", "entrance");
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,40 @@
-var gulp = require('gulp');
-var durandal = require('gulp-durandal');
-var less = require('gulp-less');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require("gulp-v3");
+var durandal = require("gulp-durandal");
+var less = require("gulp-less");
+var sourcemaps = require("gulp-sourcemaps");
 
- 
-gulp.task('durandal', function(){
-    return durandal({
-		baseDir: 'Website/app',
-		main: 'main.js',   //Same as default, so not really required. 
-		output: 'shameen.info.js',
-		almond: true,
-		minify: true
-	})
-	.pipe(gulp.dest('Website'));
-});
- 
-gulp.task('less', function () {
-  return gulp.src('Website/less/*.less')
-  .pipe(sourcemaps.init())
-  .pipe(less())
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('Website/css'));
+gulp.task("durandal", function () {
+  return durandal({
+    baseDir: "Website/app",
+    main: "main.js", //Same as default, so not really required.
+    output: "shameen.info.js",
+    almond: true,
+    minify: true,
+  }).pipe(gulp.dest("Website"));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('Website/app/**/*.js', function(event) {
-        gulp.run('scripts');
-    });
-	gulp.watch('Website/app/views/*.html', function(event) {
-        gulp.run('durandal');
-    });
-	gulp.watch('Website/less/**/*.less', function(event) {
-        gulp.run('less');
-    });
+gulp.task("less", function () {
+  return gulp
+    .src("Website/less/*.less")
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("Website/css"));
 });
 
-gulp.task('styles', ['less']);
-gulp.task('scripts', ['durandal']);
-gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task("watch", function () {
+  gulp.watch("Website/app/**/*.js", function (event) {
+    gulp.run("scripts");
+  });
+  gulp.watch("Website/app/views/*.html", function (event) {
+    gulp.run("durandal");
+  });
+  gulp.watch("Website/less/**/*.less", function (event) {
+    gulp.run("less");
+  });
+});
+
+gulp.task("styles", ["less"]);
+gulp.task("scripts", ["durandal"]);
+gulp.task("build", ["scripts", "styles"]);
+gulp.task("default", ["build", "watch"]);
