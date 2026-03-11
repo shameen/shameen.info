c31c7de479c8f3dac7dc028dae0f1f2aab32a7b7
shameen <shameen@users.noreply.github.com>
2026-03-11 11:47:59 +0000

    deploy: f73a7a6e387d8cbd9700febf699fbb35e8e5bcde

---
+++ app/viewmodels/welcome.js
@@ -0,0 +1,22 @@
+﻿define(function () {
+  var ctor = function () {
+    this.title = "Hello";
+    this.subHeader = "";
+    this.contactList = [
+      //   {
+      //     title: "Twitter",
+      //     url: "http://twitter.com/shameen_",
+      //   },
+      //   {
+      //     title: "YouTube",
+      //     url: "http://youtube.com/whatshameen",
+      //   },
+      {
+        title: "GitHub",
+        url: "https://github.com/shameen",
+      },
+    ];
+  };
+
+  return ctor;
+});
